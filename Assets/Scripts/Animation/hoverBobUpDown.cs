02ef743aa2a9906c72d44c96edf5cc2ddbe8d8d9
Gonzako <al341522@uji.es>
2020-05-24 00:33:26 +0200

    Late night submit and tweaks

--- Assets/Scripts/Animation/hoverBobUpDown.cs
+++ Assets/Scripts/Animation/hoverBobUpDown.cs
@@ -11,12 +11,14 @@ public class hoverBobUpDown : MonoBehaviour
     [SerializeField] float halfDistance = 0.5f;
     [SerializeField] float upTime = 0.2f, downTime = 0.5f;
 
+    public Ease upEs, downEs;
     public UnityEvent peak;
     public UnityEvent bottom;
+
     // Start is called before the first frame update
     void Start()
     {
-        var downTween = repositionTarget.DOLocalMoveY(repositionTarget.localPosition.y -  halfDistance, downTime).SetEase(Ease.InOutSine);
+        var downTween = repositionTarget.DOLocalMoveY(repositionTarget.localPosition.y -  halfDistance, downTime).SetEase(downEs);
         StartCoroutine(bobCycle(downTween));
     }
 
@@ -24,11 +26,11 @@ public class hoverBobUpDown : MonoBehaviour
     {
         yield return new WaitUntil(() => !lastT.IsPlaying());
         bottom.Invoke();
-        var upTween = repositionTarget.DOLocalMoveY(repositionTarget.localPosition.y + 2 * halfDistance, upTime).SetEase(Ease.InOutSine);
+        var upTween = repositionTarget.DOLocalMoveY(repositionTarget.localPosition.y + 2 * halfDistance, upTime).SetEase(upEs);
 
         yield return new WaitUntil(() => !upTween.IsPlaying());
         peak.Invoke();
-        var downTween = repositionTarget.DOLocalMoveY(repositionTarget.localPosition.y - 2 * halfDistance, downTime).SetEase(Ease.InOutSine);
+        var downTween = repositionTarget.DOLocalMoveY(repositionTarget.localPosition.y - 2 * halfDistance, downTime).SetEase(downEs);
 
 
         //Is yield return needed in this case?
